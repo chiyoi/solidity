5a10e5947df71560533b90dfd1e9a0c2daa4c4f8
chiyoi <chiyoi2140@outlook.com>
2023-12-21 21:49:50 +0800

    underflow test

--- Playground.sol
+++ Playground.sol
@@ -2,8 +2,8 @@
 pragma solidity ^0.8.20;
 
 contract Playground {
-    function main() public pure returns (uint u, int i) {
-        u = type(uint).max;
-        i = int(type(uint).max);
+    function main() public pure returns (uint) {
+        uint x = 0;
+        return x - 1;
     }
 }
